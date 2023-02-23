3c1b61563c5ab2a55280076e701a4fe5d541b258
BineeshN27 <54434843+BineeshN27@users.noreply.github.com>
2023-02-23 17:21:44 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -10,7 +10,7 @@ terraform {
     organization = "bineeshn"
     hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
     workspaces {
-      name = "terraformtest"
+      name = "terraformtest1"
     }
   }
 
