268fd47ae027c056bda3b9786b9f66c11e9fab48
BineeshN <Bineesh_N@ad.infosys.com>
2023-02-23 16:02:23 +0530

    test

--- terraform.tf
+++ terraform.tf
@@ -1,23 +1,23 @@
 terraform {
   required_providers {
     tfe = {
-      source = "hashicorp/tfe"
+      source  = "hashicorp/tfe"
       version = "0.42.0"
     }
   }
-  
+
   cloud {
-  organization = "BineeshN"
-  hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
-  workspaces {
-    name = "terraform"
-   }
-}
+    organization = "BineeshN"
+    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
+    workspaces {
+      name = "terraform"
+    }
+  }
 
 }
 
 
-# app_name = auth0-connectors
+# app_name = auth0-connectors er
 resource "tfe_workspace" "testworkspace" {
   name         = "workspace1"
   organization = "BineeshN"
