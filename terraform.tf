b2718a5278e7594832a683381308d10284bc17da
BineeshN27 <54434843+BineeshN27@users.noreply.github.com>
2023-02-23 10:17:23 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.36.1"
+    }
+  }
+
 # app_name = auth0-connectors
 resource "tfe_workspace" "testworkspace" {
   name         = "workspace1"
